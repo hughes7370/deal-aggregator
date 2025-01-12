182b1854678feb24a0156959a4d6aa5f37add2f4
Chase_hughes <99088543+hughes7370@users.noreply.github.com>
2025-01-12 19:50:58 +0100

    homepage updates

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/home/Hero";
 import Features from "@/components/home/Features";
 import Stats from "@/components/home/Stats";
 import CTASection from "@/components/home/CTASection";
-import SuccessStories from "@/components/home/SuccessStories";
+// import SuccessStories from "@/components/home/SuccessStories";
 import BrokerLogos from "@/components/home/BrokerLogos";
 import SampleDeal from "@/components/home/SampleDeal";
 import DealFlowOverview from "@/components/home/DealFlowOverview";
@@ -25,7 +25,7 @@ export default async function Home() {
       <Pricing />
       <DealFlowOverview />
       <SampleDeal />
-      <SuccessStories />
+      {/* <SuccessStories /> */}
       <CTASection />
     </main>
   );
